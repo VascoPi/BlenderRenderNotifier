3a0eec723398e6d6afb51e71773ef8d6c35ba6ed
sdilmob@gmail.com <sdilmob@gmail.com>
2019-01-02 10:19:51 +0200

    Clean code

--- blender_render_notifier.py
+++ blender_render_notifier.py
@@ -18,42 +18,18 @@ sys.path.append(os.path.dirname(__file__))
 URL = "https://api.telegram.org/bot{token}/"
 
 
-def get_token(self):
-    return self['telegram_token']
-
-
-def set_token(self, value):
-    self['telegram_token'] = value
-
-
-def get_user_id(self):
-    return self['telegram_user']
-
-
-def set_user_id(self, value):
-    self['telegram_user'] = value
-
-
-bpy.types.Scene.telegram_token = bpy.props.StringProperty(name="Token",
-                                                          maxlen=45,
-                                                          get=get_token,
-                                                          set=set_token)
-bpy.types.Scene.telegram_user = bpy.props.StringProperty(name="User ID",
-                                                         get=get_user_id,
-                                                         set=set_user_id)
-
-
 def send_message(self, text):
     token = bpy.context.user_preferences.addons[__name__].preferences.telegram_token
     chat_id = bpy.context.user_preferences.addons[__name__].preferences.telegram_user
-    url = (URL + 'sendmessage?chat_id={chat_id}&text={text}').format(
-        token=token, chat_id=chat_id, text=text)
-    requests.get(url)
+    if all([token, chat_id]):
+        url = (URL + 'sendmessage?chat_id={chat_id}&text={text}').format(token=token, chat_id=chat_id, text=text)
+        requests.get(url)
+    else:
+        pass
 
 
-@ persistent
+@persistent
 def send_message_start(self):
-    print('START')
     text = "START RENDER:\nscene: {name}\nframe: {frame}\nstarts at: {time}".format(
         name=bpy.context.scene.name,
         frame=bpy.context.scene.frame_current,
@@ -63,7 +39,6 @@ def send_message_start(self):
 
 @persistent
 def send_message_end(self):
-    print('END')
     text = "FINISH RENDER:\nscene: {name}\nframe: {frame}\nends at: {time}".format(
         name=bpy.context.scene.name,
         frame=bpy.context.scene.frame_current,
@@ -74,10 +49,8 @@ def send_message_end(self):
 # addon pref
 class BlenderRenderNotifierAddonPrefs(bpy.types.AddonPreferences):
     bl_idname = __name__
-    telegram_token = bpy.props.StringProperty(name="Token", maxlen=45,
-                                              get=get_token, set=set_token)
-    telegram_user = bpy.props.StringProperty(name="User ID", get=get_user_id,
-                                             set=set_user_id)
+    telegram_token = bpy.props.StringProperty(name="Token", maxlen=45)
+    telegram_user = bpy.props.StringProperty(name="User ID")
 
     def draw(self, context):
         layout = self.layout
